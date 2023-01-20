2b34fcb07017a80c6f7045e5287721de6c3e417e
Lakindu Widuranga Alwis <vidurangalakindu@gmail.com>
2023-01-21 01:36:56 +0530

    Add files via upload

---
+++ src/main/java/com/example/GameScores/Controller/userController.java
@@ -0,0 +1,14 @@
+package com.example.GameScores.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class userController {
+
+    public void createUser(){ //Create a user - POST
+        
+    }
+
+}
